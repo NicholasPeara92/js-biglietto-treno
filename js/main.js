36838d8ebf2097dfdb7963dde8853449e0d1d8c0
NicholasPeara92 <nicholas.peara@gmail.com>
2022-10-20 15:36:55 +0200

    Inserisce condizione per non inserire dati negativi

--- js/main.js
+++ js/main.js
@@ -2,9 +2,21 @@
 
 const distance = Number(prompt("Inserisci in chilometri la distanza che desideri percorrere"));
 
+
 // chiedere all'utente la sua età
 
 const age = Number(prompt( "Inserisci la tua età"));
+
+// imporre condizione per cui l'utente non può inserire un numero inferiore o uguale a 0 nei dati
+
+if (distance <= 0 || age <= 0) {
+    alert("Attenzione, il numero non può essere inferiore o uguale a 0. Verrà ricaricata la pagina");
+    window.location.reload();
+}
+
+// imporre condizione per cui l'utente non può inserire dei dati non numerici
+
+
 
 // inserire variabile del prezzo base del biglietto
 
@@ -19,12 +31,8 @@ if (age < 18) {
 } else if (age >= 65) {
     // 1b. altrimenti se l'utente ha un età maggiore o uguale ai 65 anni viene applicato uno sconto del 40%
     ticket = ticket - (ticket * 0.4);
-
-} else {
-    // 1c. altrimenti il prezzo del biglietto è quello base
-    ticket= ticket;
-        
 }
+
 // prezzo totale del biglietto con eventuale sconto applicato
 
 console.log('il prezzo del biglietto è:', ticket.toFixed (2), '€');
